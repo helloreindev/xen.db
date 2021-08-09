0ac256bf1f3c889712ef0b86d5b749d6e9112126
Reinhardt <notmarx.tech@gmail.com>
2021-08-09 12:18:47 +0700

    Fix typo

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
  * This is an Open-Sourced Easy-To-Use SQLite3 Database Designed Persistently For Beginners & Advanced Developer.
  * All Data Are Stored Locally In A File In The Same Project.
  * 
- * For Issues, Please Check Out: https://github.com/reinhardt/xen.db/issues
+ * For Issues, Please Check Out: https://github.com/NotMarx/xen.db/issues
  */
 
  module.exports = {
